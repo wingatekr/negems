8072865114257e5813676dbbab24d92a9590dbef
wingatekr <wingatekr@gmail.com>
2017-08-14 21:20:22 -0400

    application files part one

---
+++ scripts/MacroPartials/birthstones.js
@@ -0,0 +1,11 @@
+﻿(function ($) {
+    $(".birthstone-name > a").on("click", function () {
+        var $this = $(this);
+        var $parent = $this.parent();
+        var $panel = $(".birthstone-description." + $this.text());
+        $parent.siblings().removeClass("active");
+        $parent.addClass("active");
+        $panel.siblings().removeClass("active");
+        $panel.addClass("active");
+    }).first().trigger("click");
+})(jQuery)
